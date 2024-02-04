6a221da9c2c3cefa8262cb08eb2f922a00c528fc
remolan1213 <75658407+remolan1213@users.noreply.github.com>
2024-02-04 20:27:46 +0800

    new edits

--- src/Application.js
+++ src/Application.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import HighScore from "./HighScore";
 
 class Application extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      overTen: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  componentDidUpdate(props, state) {
+    if (
+      this.state.count > 10 &&
+      this.state.count != state.count &&
+      !this.state.overTen
+    ) {
+      console.log("Updating over ten");
+      this.setState({ overTen: true });
     }
+  }
+
+  resetCount = (e) => {this.setState({ count: 0, overTen: false })
+  }
+  render() {
+    let { count } = this.state;
+    return (
+      <div>
+        <h1>You clicked button {count} times.</h1>
+        <HighScore 
+        overTen={this.state.overTen} 
+        onReset={this.resetCount}
+        />
+        <span>
+          <button onClick={() => this.handleClick()}>Click Me</button>
+        </span>
+      </div>
+    );
+  }
 }
 
 export default Application;
